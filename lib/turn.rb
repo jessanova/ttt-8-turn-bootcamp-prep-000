55fd0598cb893b8b28bc052085c71a6d11757892
Jess Jacobson <jessanova@gmail.com>
2018-10-16 19:20:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,15 @@ def display_board
   
 end
 
-def valid_move?
-  
+def valid_move?(board,index)
+  if(index.between?(0,8))
+    return true
+  end
 end
 
-def move
-  
+def move(board, input)
+  input-=1
+  if(valid_move?(board,input))
+    
+  end
 end
