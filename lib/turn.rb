310a1eb84ea175877dda177bf5dc0e7b744706c4
Jess Jacobson <jessanova@gmail.com>
2018-10-16 19:24:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,9 @@
 def display_board
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board,index)
