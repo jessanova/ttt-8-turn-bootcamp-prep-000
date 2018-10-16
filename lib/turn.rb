4032293b03e778bf322e350f1718789168009483
Jess Jacobson <jessanova@gmail.com>
2018-10-16 19:22:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,9 @@ end
 
 def valid_move?(board,index)
   if(index.between?(0,8))
-    if(board[index]==" ")
+    if( (board[index]==" ") || (board[index].to_s.empty?) || (board[index].nil?))
+      return TRUE
+    end
   end
   return false
 end
@@ -12,6 +14,6 @@ end
 def move(board, input)
   input-=1
   if(valid_move?(board,input))
-    
+    board[input]="X"
   end
 end
